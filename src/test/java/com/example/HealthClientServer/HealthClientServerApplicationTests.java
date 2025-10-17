2e0e7175692b5b8e11c799df8e7bbe86f3b08adb
Flavius10 <flaviusa1010@gmail.com>
2025-10-17 14:12:37 +0300

    Start the project

---
+++ src/test/java/com/example/HealthClientServer/HealthClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthClientServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthClientServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
