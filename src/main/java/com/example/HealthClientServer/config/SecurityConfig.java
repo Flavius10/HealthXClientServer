5a484e197d9d599d401572adcafde6dbb014efd2
Flavius10 <flaviusa1010@gmail.com>
2025-10-23 11:20:33 +0300

    Implement the SecurityConfig and OAuth2AuthorizedClientManager for managing the receive of the acces tokens

---
+++ src/main/java/com/example/HealthClientServer/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.HealthClientServer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.oauth2Client(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/data"));
+
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository
+    ) {
+
+        OAuth2AuthorizedClientProvider auth2AuthorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .build();
+
+        var authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository
+        );
+
+        authorizedClientManager.setAuthorizedClientProvider(auth2AuthorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
+}
